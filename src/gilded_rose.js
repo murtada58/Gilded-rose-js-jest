e3801af222cd0a035c4aca98f651ed45998af092
Murtada Alzubaidi <murtada.alzubaidi@softwire.com>
2022-04-08 18:14:04 +0100

    Second refactor

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,63 +1,82 @@
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 }
 
 class Shop {
-  constructor(items=[]){
-    this.items = items.map(item => this.keepWithinBounds(item));
-  }
+    constructor(items = []) {
+        items.forEach((item) => this.keepItemQualityWithinBounds(item));
+        this.items = items;
+    }
 
-  updateAgedBrieQuality(item) {
-    item.quality++;
-    return item;
-  }
+    updateAllItems() {
+        this.items.forEach((item) => {
+            item.sellIn--;
+            this.updateItemQuality(item);
+        });
+    }
 
-  updateBackstagePassQuality(item) {
-    item.quality++;
-    if (item.sellIn <= 10) { item.quality++; }
-    if (item.sellIn <= 5) { item.quality++; }
+    keepItemQualityWithinBounds(item) {
+        if (item.name.includes("Sulfuras")) {
+            return;
+        }
+        item.quality = Math.min(50, Math.max(item.quality, 0));
+    }
 
-    if (item.sellIn < 0) { item.quality = 0; }
-    return item;
-  }
+    updateItemQuality(item) {
+        update(item);
+        this.keepItemQualityWithinBounds(item);
 
-  updateGeneralItemQuality(item) {
-    let decrement = 1;
-    if (item.name.startsWith("Conjured")) { decrement *= 2 };
+        function update(item) {
+            if (item.name.includes("Sulfuras")) {
+                return;
+            }
 
-    if (item.sellIn < 0) { item.quality -= 2 * decrement; }
-    else { item.quality -= decrement; }
-    
-    return item;
-  }
+            if (item.name.includes("Backstage pass")) {
+                updateBackstagePassQuality(item);
+                return;
+            }
 
-  keepWithinBounds(item) {
-    if (item.name.startsWith("Sulfuras")) { return item; }
-    item.quality = Math.min(50, Math.max(item.quality, 0));
-    return item;
-  }
+            if (item.name === "Aged Brie") {
+                updateGeneralItemQuality(item, 1);
+                return;
+            }
 
-  updateQuality() {
-    this.items = this.items.map(item => {
-      item.sellIn--;
-      if (item.name === "Aged Brie") { return this.updateAgedBrieQuality(item); }
+            updateGeneralItemQuality(item, -1);
 
-      if (item.name.startsWith("Sulfuras")) { return item; }
-      
-      if (item.name.startsWith("Backstage pass")) { return this.updateBackstagePassQuality(item); }
+            function updateBackstagePassQuality(item) {
+                item.quality++;
+                if (item.sellIn <= 10) {
+                    item.quality++;
+                }
+                if (item.sellIn <= 5) {
+                    item.quality++;
+                }
 
-      return this.updateGeneralItemQuality(item);
-    })
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            }
 
-    this.items = this.items.map(item => this.keepWithinBounds(item));
-  }
+            function updateGeneralItemQuality(item, changeAmount) {
+                if (item.name.includes("Conjured")) {
+                    changeAmount *= 2;
+                }
+
+                if (item.sellIn < 0) {
+                    item.quality += 2 * changeAmount;
+                } else {
+                    item.quality += changeAmount;
+                }
+            }
+        }
+    }
 }
 
 module.exports = {
-  Item,
-  Shop
-}
+    Item,
+    Shop,
+};
